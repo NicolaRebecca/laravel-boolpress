87e79f6ed0bdef7a2d1f12a983f069ea65cd7d98
NicolaRebecca <master9895@gmail.com>
2021-09-13 19:36:13 +0200

    es.

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 50; $i++){
+            $post = new Post();
+            $post -> author = $faker -> words(2,true);
+            $post -> content = $faker -> paragraph(5);
+            $post -> img_path = $faker -> imageUrl(640,480, 'cars', true);
+            $post -> save();
+        }
+    }
+}
