f25829a51a4164978dc5dcc82c35979adeb8276c
NicolaRebecca <master9895@gmail.com>
2021-09-23 09:43:33 +0200

    category and postDetail

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -14,10 +14,23 @@ class PostsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         for($i = 0; $i < 50; $i++){
+
+            // $postDetail = new PostDetail();
+            // $postDetail -> type = $faker->words(1,true);
+            // $postDetail -> author_name = $faker->words(1,true);
+            // $postDetail -> save();
+            
+
+
+
             $post = new Post();
             $post -> author = $faker -> words(2,true);
             $post -> content = $faker -> paragraph(5);
             $post -> img_path = $faker -> imageUrl(600,400, 'post', true);
+
+            // $post->Post_detail_id = $postDetail -> id;
+
+
             $post -> save();
         }
     }
